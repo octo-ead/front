bd75a86ee6a98369d540df10cfcbebebfab69408
C@irx <cristian.ibanez@aluno.unila.edu.br>
2019-06-07 16:49:01 -0300

    Adicionando estilos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,45 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/styles";
-import Logo from "../../assets/images/octo-ead-2-coral.fw.png";
+import { AppBar, Toolbar, Typography, Button, Grid, Card, CardActions, CardContent, CardMedia, Container} from "@material-ui/core";
+import Logo from "../assets/images/octo-ead-2-coral.fw.png";
 
 const useStyles = makeStyles({
   root: {
-    display: "grid",
-    "grid-template-columns": "1fr",
-    backgroundColor: "#FF7F50",
-    height: "100vh",
-    justifyContent: "center",
-    justifyItems: "center",
-    alignContent: "center"
+    height: "100%",
+    backgroundColor: "#222931",
+    padding: "100px 10px",
+  },
+  barDark:{
+    background: "#464646",
   },
   bigLogo: {
-    width: 180,
-    height: 180
-  }
+    width: 60,
+    height: 60,
+    marginRight: "10px",
+  },
+  button:{
+    backgroundColor: "white",
+  },
+  card:{
+    backgroundColor: "transparent",
+    border: "3px solid #464646",
+    textAlign: "center",
+    color: "#fff",
+  },
+  cardMedia:{
+    height: "100px",
+    marginTop: "15px",
+  },
+  heroContainer:{
+    marginBottom: "50px",
+    textAlign: "center",
+    color: "#fff",
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: "bold",
+    fontSize: "2rem",
+  },
 });
 
 export const Home = () => {
